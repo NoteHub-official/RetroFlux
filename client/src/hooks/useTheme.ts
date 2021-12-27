fd56fc973e1b7607d0d234c010f3446baccddaa4
Toubat <57741529+Toubat@users.noreply.github.com>
2021-12-27 15:41:56 -0600

    update useTheme hook

--- client/src/hooks/useTheme.ts
+++ client/src/hooks/useTheme.ts
@@ -1,16 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 type Theme = "light" | "dark";
 
 const storageKey = "notehub-theme";
 const initialTheme: Theme = localStorage.getItem(storageKey) ? "dark" : "light";
+const root = document.getElementById("root");
 
+/**
+ * Hooks that returns the current theme and a function to set the theme
+ * between light and dark.
+ * @returns [theme: Theme, toggleTheme: () => void]
+ */
 const useTheme = () => {
   const [theme, setTheme] = useState<Theme>(initialTheme);
 
+  useEffect(() => {
+    if (initialTheme === "dark" && root && !root.classList.contains("dark")) {
+      root.classList.add("dark");
+    }
+  }, []);
+
   const toggleTheme = (): void => {
     const currentTheme = theme;
-    const root = document.getElementById("root");
 
     if (root) {
       if (currentTheme === "dark") {
