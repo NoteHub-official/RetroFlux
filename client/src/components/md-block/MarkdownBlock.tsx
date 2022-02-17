a1d7121d92778f2dcbe1b009b763e3bafcd7430a
Boquan (Brian) Yin <57741529+Toubat@users.noreply.github.com>
2022-02-17 03:45:56 -0600

    update: emm

--- client/src/components/md-block/MarkdownBlock.tsx
+++ client/src/components/md-block/MarkdownBlock.tsx
@@ -4,12 +4,47 @@ import { Textarea, useColorModeValue, Code } from "@chakra-ui/react";
 // plugins
 import remarkGfm from "remark-gfm";
 import remarkMath from "remark-math";
-import rehypeMathjax from "rehype-mathjax";
 import rehypeKatex from "rehype-katex";
+import remarkMdx from "remark-mdx";
 import cx from "classnames";
 import * as Y from "yjs";
 import { WebrtcProvider } from "y-webrtc";
 import { IndexeddbPersistence } from "y-indexeddb";
+import { MDXProvider } from "@mdx-js/react";
+import { compile } from "@mdx-js/mdx";
+import { visit } from "unist-util-visit";
+
+function myRemarkPlugin() {
+  /** @param {import('@types/mdast').Root} tree */
+  return (tree: any) => {
+    visit(tree, (node) => {
+      // `node` can now be one of the nodes for JSX, expressions, or ESM.
+      console.log(node);
+    });
+  };
+}
+
+class ErrorBoundary extends React.Component<any, any> {
+  constructor(props: any) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error: any) {
+    // Update state so the next render will show the fallback UI.
+    console.log(error);
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
 
 export interface MarkdownBlockProps {}
 
@@ -47,42 +82,51 @@ export const MarkdownBlock: React.FC<MarkdownBlockProps> = (props) => {
   return (
     <div className="md-block min-w-full prose">
       <Textarea value={markdownText} onChange={handleChange} height="xs" />
-      <ReactMarkdown
-        children={markdownText}
-        remarkPlugins={[remarkGfm, remarkMath]}
-        rehypePlugins={[rehypeKatex]}
-        components={{
-          code: (props) => <CodeElement {...props} />,
-          // @ts-ignore
-          div: (props) => {
-            const { children, node, className } = props;
-            const margin = className?.split(" ").includes("math-display");
-            return <div className={cx(className, { "flex justify-center": margin })}>{children}</div>;
-          },
-          // span: (props) => {
-          //   const { children, node, className } = props;
-          //   const margin = className?.split(" ").includes("math-inline");
-
-          //   return <span className={cx(className, { "mx-1": margin }, "text-base")}>{children}</span>;
-          // },
-          // p: (props) => {
-          //   const { children, node, className } = props;
-
-          //   return <p className={cx(className, "flex items-center")}>{children}</p>;
-          // },
-          // li: (props) => {
-          //   const { children, node, className } = props;
-
-          //   return <li className={cx(className, "flex items-center")}>{children}</li>;
-          // },
-        }}
-      />
+      {/* <MDXProvider>
+        <div style={{ color: "red" }}>sadasda</div>
+      </MDXProvider> */}
+      <ErrorBoundary markdownText={markdownText}>
+        <ReactMarkdown
+          children={markdownText}
+          skipHtml
+          sourcePos
+          linkTarget={"_blank"}
+          remarkPlugins={[remarkMdx, remarkGfm, remarkMath, myRemarkPlugin, remarkMdx]}
+          rehypePlugins={[rehypeKatex]}
+          transformLinkUri={(href, children, title) => {
+            return title || "";
+          }}
+          components={{
+            // @ts-ignore
+            div: (props) => {
+              const { children, node, className } = props;
+              const margin = className?.split(" ").includes("math-display");
+              return <div className={cx(className, { "flex justify-center": margin })}>{children}</div>;
+            },
+            h1: (props) => (
+              <h1 {...props} className="text-4xl font-bold border-b-[1px] pb-2">
+                {props.children}
+              </h1>
+            ),
+            h2: (props) => (
+              <h2 {...props} className="text-3xl font-bold border-b-[1px] pb-2">
+                {props.children}
+              </h2>
+            ),
+            h3: (props) => (
+              <h3 {...props} className="text-2xl font-bold border-b-[1px] pb-2">
+                {props.children}
+              </h3>
+            ),
+            blockquote: (props) => {
+              const { children, node, className } = props;
+              return (
+                <blockquote className={cx(className, "whitespace-pre-wrap")}>{children}</blockquote>
+              );
+            },
+          }}
+        />
+      </ErrorBoundary>
     </div>
   );
-};
-
-const CodeElement = (props: any) => {
-  const color = useColorModeValue("blue.400", "red.400");
-
-  return <Code {...props} className="bg-gray-200 p-1 rounded-md" color={color} />;
 };
